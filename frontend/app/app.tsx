0bd90c20d50172df21004dbfb500d974d335c69c
Nischal Khanal <khanalnischal2075@gmail.com>
2024-10-24 02:03:12 +0545

    enh: get sever-side props (getSession)

---
+++ frontend/app/app.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+import Login from "./components/login";
+
+export default function App() {
+    // 2. Wrap ChakraProvider at the root of your app
+    return (
+        <ChakraProvider>
+            <Login />
+        </ChakraProvider>
+    );
+}
--- frontend/app/components/login.tsx
+++ frontend/app/components/login.tsx
@@ -1,13 +1,26 @@
-"use client";
-import { signIn, signOut } from "next-auth/react";
-import { useSession } from "next-auth/react";
-import GoogleButton from "react-google-button";
-
-export default function Login() {
-    const { data: session } = useSession();
+import { signIn, signOut } from "@/auth";
+import getSession from "../lib/getSession";
+export default async function Login() {
+    const session = await getSession();
+    console.log("Here is the data ", session);
     return session?.user?.name == null ? (
-        <GoogleButton onClick={() => signIn("google")}>Sign In</GoogleButton>
+        <form
+            action={async () => {
+                "use server";
+                await signIn("google");
+            }}
+        >
+            <button>Sign In</button>
+        </form>
     ) : (
-        <button onClick={() => signOut()}> Sign Out {session?.user?.name}   </button>
+        <form
+            action={async () => {
+                "use server";
+                await signOut();
+            }}
+        >
+        
+            <button>Sign Out {session?.user?.name}</button>
+        </form>
     );
 }
